e2e66a4a3c919761d95c221201d09c5ee1ea051b
unknown <louha@mail.com>
2021-03-23 10:11:22 +0100

    commit signup + signin

--- myapp/src/main/java/fr/cesi/TheMask/view/IndexView.java
+++ myapp/src/main/java/fr/cesi/TheMask/view/IndexView.java
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/Index")
 public class IndexView extends ViewBase implements ViewInterface {
     static final long serialVersionUID = -1;
 
